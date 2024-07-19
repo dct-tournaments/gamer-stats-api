bd705bfa5a6856360cb108479f634883d7421519
Karine Valença <valenca.karine@gmail.com>
2024-07-19 10:57:33 +0200

    feat(league of legends): add br1 platform routing

--- pkg/leagueoflegends/region.go
+++ pkg/leagueoflegends/region.go
@@ -5,14 +5,18 @@ type RegionalRouting string
 
 const (
 	EUW1PlatformRouting PlatformRouting = "EUW1"
+	BR1PlatformRouting  PlatformRouting = "BR1"
 
-	EuropeRegionalRouting RegionalRouting = "europe"
+	EuropeRegionalRouting   RegionalRouting = "europe"
+	AmericasRegionalRouting RegionalRouting = "americas"
 )
 
 func (s service) fromPlatformRoutingToRegionalRouting(platformRouting PlatformRouting) RegionalRouting {
 	switch platformRouting {
 	case EUW1PlatformRouting:
 		return EuropeRegionalRouting
+	case BR1PlatformRouting:
+		return AmericasRegionalRouting
 	default:
 		return EuropeRegionalRouting
 	}
